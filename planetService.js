57e8ce35ee51ead31cf6b3ed17f5b155c4041866
Karisv <karis15@ru.is>
2018-11-06 22:15:13 +0000

    lala

--- planetService.js
+++ planetService.js
@@ -22,41 +22,47 @@ class PlanetService extends EventEmitter  {
     };
     
     getCoordinatesByPlanetId(id) {
-        Coordinate.findById(id, (err, data) => {
+        Coordinate.find(id, (err, data) => {
             if (!data) { this.emit('error', { statusCode: 404, message: 'Not found' }); }
             else if (err) { this.emit('error', { statusCode: 500, message: err }); } 
             else { this.emit(this.events.GET_ALL_PLANTES_ID, data); }
         })
     }
     
-    addCoordinates(Coordinate) {
-        Coordinate.findById(Coordinate.planedId, (err, data) => {
+   /* addCoordinates(coordinate) {
+        Coordinate.find({planetId: id}, (err, data) => {
             if (!data) { this.emit('error', { statusCode:400, message: 'Planet does not exist' }); }
             else if (err) { this.emit('error', { statusCode: 500, message: err }); }
             else {
                 Coordinate.create()
             }
         })
-    }
+    }*/
 
-        /*addCoordinates = (id, body) => {
-        return new Promise((resolve, reject) => {
+        addCoordinates(id, body){
+        //return new Promise((resolve, reject) => {
             Coordinate.find({ planetId: id }, (err, coordinates) => {
                 if (!coordinates) {
                     this.emit('error', { statusCode: 400, message: 'Planet doesn' })
-                }
-                console.log(body);
-                let input = {
-                    planetId: id,
-                    latitude: body.latitude,
-                    longitude: body.longitude
+                } else if (err) { this.emit('error', { statusCode: 500, message: err }); }
+                else { 
+                    console.log(body);
+                    let input = {
+                        planetId: id,
+                        latitude: body.latitude,
+                        longitude: body.longitude
+                    }
+                    
+                    console.log(input);
+                    Coordinate.create(input, (err, body) => {
+                        if(err) { this.emit('error', { statusCode: 500, message: err }); }
+                        else { this.emit(this.events.CREATE_COORDINATES); }
+                    });
                 }
                 
-                console.log(input);
-                Coordinate.create(input);
             })
-        })
-    };*/
+        //})
+    };
     
 }
 
