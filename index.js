0c673a62305ae02e7dbc9bfb44244d06048a81d5
Johann Egilsson <johannegi16@ru.is>
2018-11-06 22:32:43 +0000

    fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var app = express()
 var service = require('./planetService.js')
 
 // GET all planets
-router.get('/planets', (req, res) => { 
+router.get('/planets', (req, res) => {
   const planetService = new service();
   planetService.on('error', err => res.status(err.statusCode).send(err.message))
   planetService.on(planetService.events.GET_ALL_PLANETS, data => res.json(data));
@@ -15,25 +15,28 @@ router.get('/planets', (req, res) => {
   //service.getAllPlanets().then(resp => {return res.status(200).send(resp)}) 
 })
 
-router.get('/planets/:planetId/coordinates', (req, res) => { 
+router.get('/planets/:id/coordinates', (req, res) => {
   const planetService = new service();
-  const {planetId} = req.param;
+  const { id } = req.params;
   planetService.on('error', err => res.status(err.statusCode).send(err.message));
   planetService.on(planetService.events.GET_ALL_PLANTES_ID, data => res.json(data));
-  planetService.getCoordinatesByPlanetId(planetId);
+
+  planetService.getCoordinatesByPlanetId(id);
   //const {planetId} = req.params
   //service.getCoordinatesByPlanetId(planetId).then(resp => {return res.status(200).send(resp)}) 
 })
 
-router.post('/planets/:planetId/coordinates', (req, res) => {
+router.post('/planets/:id/coordinates', (req, res) => {
   const planetService = new service();
-  const {planetId} = req.param;
+  const { id } = req.params;
   const reqBody = req.body;
+  planetService.on('error', err => res.status(err.statusCode).send(err.message));
   planetService.on(planetService.events.CREATE_COORDINATES, function () {
     res.status(200).send('Created Successfully');
   });
-  planetService.addCoordinates(planetId, reqBody);
-  
+  console.log(req.param);
+  planetService.addCoordinates(id, reqBody);
+
   //const {planetId} = req.params
   //const reqBody = req.body
   //service.addCoordinates(planetId, reqBody).then(resp => {return res.status(201).send(resp)})
@@ -47,5 +50,5 @@ if (portA == null || portA == "") {
   portA = port;
 }
 app.listen(portA, () => {
-	console.log(`Listening on port ${portA}`);
+  console.log(`Listening on port ${portA}`);
 });
--- planetService.js
+++ planetService.js
@@ -3,7 +3,7 @@ const { Planet } = require('./data/db.js')
 const { Coordinate } = require('./data/db.js')
 
 
-class PlanetService extends EventEmitter  {
+class PlanetService extends EventEmitter {
     constructor() {
         super();
         this.events = {
@@ -20,50 +20,52 @@ class PlanetService extends EventEmitter  {
             else { this.emit(this.events.GET_ALL_PLANETS, planets); }
         });
     };
-    
+
     getCoordinatesByPlanetId(id) {
-        Coordinate.find(id, (err, data) => {
+        console.log(id);
+        Coordinate.find({ planetId: id }, (err, data) => {
+            console.log(id);
             if (!data) { this.emit('error', { statusCode: 404, message: 'Not found' }); }
-            else if (err) { this.emit('error', { statusCode: 500, message: err }); } 
+            else if (err) { this.emit('error', { statusCode: 500, message: err }); }
             else { this.emit(this.events.GET_ALL_PLANTES_ID, data); }
         })
     }
-    
-   /* addCoordinates(coordinate) {
-        Coordinate.find({planetId: id}, (err, data) => {
-            if (!data) { this.emit('error', { statusCode:400, message: 'Planet does not exist' }); }
-            else if (err) { this.emit('error', { statusCode: 500, message: err }); }
-            else {
-                Coordinate.create()
-            }
-        })
-    }*/
 
-        addCoordinates(id, body){
+    /* addCoordinates(coordinate) {
+         Coordinate.find({planetId: id}, (err, data) => {
+             if (!data) { this.emit('error', { statusCode:400, message: 'Planet does not exist' }); }
+             else if (err) { this.emit('error', { statusCode: 500, message: err }); }
+             else {
+                 Coordinate.create()
+             }
+         })
+     }*/
+
+    addCoordinates(id, body) {
         //return new Promise((resolve, reject) => {
-            Coordinate.find({ planetId: id }, (err, coordinates) => {
-                if (!coordinates) {
-                    this.emit('error', { statusCode: 400, message: 'Planet doesn' })
-                } else if (err) { this.emit('error', { statusCode: 500, message: err }); }
-                else { 
-                    console.log(body);
-                    let input = {
-                        planetId: id,
-                        latitude: body.latitude,
-                        longitude: body.longitude
-                    }
-                    
-                    console.log(input);
-                    Coordinate.create(input, (err, body) => {
-                        if(err) { this.emit('error', { statusCode: 500, message: err }); }
-                        else { this.emit(this.events.CREATE_COORDINATES); }
-                    });
+        Coordinate.find({ planetId: id }, (err, coordinates) => {
+            if (!coordinates) {
+                this.emit('error', { statusCode: 400, message: 'Planet doesn' })
+            } else if (err) { this.emit('error', { statusCode: 500, message: err }); }
+            else {
+                console.log(body);
+                let input = {
+                    planetId: id,
+                    latitude: body.latitude,
+                    longitude: body.longitude
                 }
-                
-            })
+
+                console.log(input);
+                Coordinate.create(input, (err, body) => {
+                    if (err) { this.emit('error', { statusCode: 500, message: err }); }
+                    else { this.emit(this.events.CREATE_COORDINATES); }
+                });
+            }
+
+        })
         //})
     };
-    
+
 }
 
 module.exports = PlanetService;
